504c72860c0a2d0688614f263150a7a2ad3ce90e
nadiaS11 <nadiasultana.ns11@gmail.com>
2023-10-03 15:29:29 +0600

    first commit

---
+++ src/Router/Router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <div className="font-poppins text-4xl">Hello world!</div>,
+  },
+]);
+
+export default routes;
